1c6099d16a04c981762dd61a3da746aa03841295
Anime <franciscotadeuuu@gmail.com>
2025-09-29 14:32:52 -0300

    Initial commit
    Generated by create-expo-app 3.5.3.

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+import { useState } from 'react';
+export default function HomeScreen() {
+  return (
+   
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
